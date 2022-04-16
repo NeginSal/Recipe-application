fa0efe41bdd5e30ec2ecb75b16ddd0e881f035fd
NeginSal <neginsaljooghi@gmail.com>
2022-04-16 09:58:53 +0430

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="create" element={<Create />}></Route>
+          <Route path="search" element={<Search />}></Route>
+          <Route path="/recipes/:id" element={<Recipe />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
