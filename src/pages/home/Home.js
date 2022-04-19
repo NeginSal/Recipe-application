da15ffa2c827586b9676b9b55a6b6b4a1c188e45
NeginSal <neginsaljooghi@gmail.com>
2022-04-19 16:12:07 +0430

    fetching data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,17 @@
+import {useFetch} from '../../hooks/useFetch'
 import './Home.css'
 
 const Home = () => {
-    return ( 
-        <div>
-            Home
+    const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
+    return (
+        <div className='home'>
+            {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>Loading ...</p>}
+            {data&& data.map(recipe=>(
+                <h2 key={recipe.id}>{recipe.title}</h2>
+            ))}
         </div>
-     );
+    );
 }
- 
+
 export default Home;
