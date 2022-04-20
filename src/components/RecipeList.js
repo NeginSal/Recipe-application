2201ce79aba7d8dc9b136993e32ae4509f36ec84
NeginSal <neginsaljooghi@gmail.com>
2022-04-20 16:17:54 +0430

    fetching a single recipe

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 
+
 // styles
 import './RecipeList.css'
 
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,11 +1,21 @@
+import { useFetch } from '../../hooks/useFetch'
+import { useParams } from "react-router-dom";
+
 import './Recipe'
 
+
 const Recipe = () => {
-    return ( 
-        <div>
-            Recipe
+    const { id } = useParams();
+    const url = 'http://localhost:3000/recipes/' + id
+    const { data: recipe, isPending, error } = useFetch(url)
+
+    return (
+        <div className='recipe'>
+            {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>Loading ...</p>}
+            {recipe && <h1>{recipe.title}</h1>}
         </div>
-     );
+    );
 }
- 
+
 export default Recipe;
