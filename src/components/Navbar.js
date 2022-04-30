6d017d9dd636e8a9265597f138469bd9b753e495
NeginSal <neginsaljooghi@gmail.com>
2022-04-30 11:05:51 +0430

    reducers and useReducers

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom';
 import './Navbar.css'
 import Searchbar from './Searchbar';
-import {useTheme} from '../hooks/useTheme'
+import { useTheme } from '../hooks/useTheme'
 
 const Navbar = () => {
 
-    const { color } = useTheme()
+    const { color, changeColor } = useTheme()
 
     return (
         <div className='navbar' style={{ background: color }}>
-            <nav>
+            <nav onClick={() => changeColor('pink')}>
                 <Link to="/" className='brand'><h1>Cooking Ninja</h1></Link>
                 <Searchbar />
                 <Link to="/create">Create Recipe</Link>
