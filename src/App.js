f11a0a21890150f1aea42edfcb26a3906a84452b
NeginSal <neginsaljooghi@gmail.com>
2022-05-01 10:08:09 +0430

    making a color theme selector

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Recipe from './pages/recipe/Recipe'
 
 import './App.css'
 import Navbar from "./components/Navbar";
+import ThemeSelector from "./components/ThemeSelector";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar/>
+      <ThemeSelector/>
       <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="create" element={<Create />}></Route>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,11 @@ import { useTheme } from '../hooks/useTheme'
 
 const Navbar = () => {
 
-    const { color, changeColor } = useTheme()
+    const { color} = useTheme()
 
     return (
         <div className='navbar' style={{ background: color }}>
-            <nav onClick={() => changeColor('pink')}>
+            <nav>
                 <Link to="/" className='brand'><h1>Cooking Ninja</h1></Link>
                 <Searchbar />
                 <Link to="/create">Create Recipe</Link>
